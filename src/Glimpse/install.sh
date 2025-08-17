4b0e811123619a4f655a0838b43101414fecb99e
jmis <324872+jmis@users.noreply.github.com>
2025-08-16 21:55:07 -0400

    Fixes

--- src/Glimpse/install.sh
+++ src/Glimpse/install.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
+scriptDirectory=$(dirname $0)
 installationDirectory="$HOME/.local/bin/"
 
-echo
-echo "### Creating ${installationDirectory}"
+# Creating ${installationDirectory}"
 mkdir $installationDirectory
 
-echo
-echo "### Copy binary to ${installationDirectory}"
-mv ./glimpse $installationDirectory
+# Copy binary to ${installationDirectory}"
+mv $scriptDirectory/glimpse $installationDirectory
 
-echo
-echo "### Updating SUPER_L shortcut"
+# Updating SUPER_L shortcut"
 xfconf-query \
 	-c xfce4-keyboard-shortcuts \
 	-p "/commands/custom/<Primary>Escape" \
@@ -19,9 +17,7 @@ xfconf-query \
 	-t string \
 	-s "gdbus call --session --dest org.glimpse --object-path /org/glimpse --method org.gtk.Actions.Activate \"OpenStartMenu\" [] {}"
 
-echo
-echo "### Disabling XFCE panel and adding Glimpse to the failsafe session (the default X session)"
-
+# Disabling XFCE panel and adding Glimpse to the failsafe session (the default X session)"
 xfceStartupNumApps=$(xfconf-query -c xfce4-session -p /sessions/Failsafe/Count)
 
 for ((i=0 ; i<$xfceStartupNumApps; i++)); do
@@ -32,12 +28,10 @@ for ((i=0 ; i<$xfceStartupNumApps; i++)); do
 	fi
 done
 
-echo
-echo "### Stopping XFCE panel"
+# Stopping XFCE panel"
 pkill -9 xfce4-panel
 
-echo
-echo "### Disable xfce4-notifyd.service"
+# Disable xfce4-notifyd.service"
 systemctl --user mask xfce4-notifyd.service
 pkill -9 xfce4-notifyd
 tee ~/.config/autostart/xfce4-notifyd.desktop << EOF
@@ -45,8 +39,7 @@ tee ~/.config/autostart/xfce4-notifyd.desktop << EOF
 Hidden=true
 EOF
 
-echo
-echo "### Disable ayatana-indicator-application.service"
+# Disable ayatana-indicator-application.service"
 systemctl --user mask ayatana-indicator-application
 pkill -9 ayatana-indicat
 tee ~/.config/autostart/ayatana-indicator-application.desktop << EOF
@@ -54,7 +47,6 @@ tee ~/.config/autostart/ayatana-indicator-application.desktop << EOF
 Hidden=true
 EOF
 
-echo
 echo "Installation complete"
 echo "*** If you use a saved X session then you will need to save a new one with Glimpse running"
 
